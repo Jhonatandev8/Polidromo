0be74b0a79d19501c374b0a4a061f8f651a336c5
Jhonatan de Paula <jhonatandev8@gmail.com>
2024-06-26 15:22:34 -0300

    primeiro

---
+++ polidromos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+
+public class Solution
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escreva o valor: ");
+        var x = Convert.ToInt32(Console.ReadLine()) ;
+        Console.WriteLine(IsPolindrome(x));
+
+    }
+
+    public static bool IsPolindrome(int x )
+    {
+        
+        var y = x.ToString().ToCharArray();
+        Array.Reverse(y);
+        var ReverString = new string(y);
+        return x.ToString() == ReverString;
+    }
+}
